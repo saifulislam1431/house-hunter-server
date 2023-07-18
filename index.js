4294d8e5695d81d5cb317ba9eb922cbe74628015
Saiful Islam <sakibbhaiya143@gmail.com>
2023-07-18 22:01:33 +0600

    Registration, Login, and user details api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
@@ -11,6 +12,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 app.use(cors());
 app.use(express.json())
 
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -94,6 +110,89 @@ async function run() {
       }).toArray()
       res.send(result)
     })
+
+    // User registration
+app.post('/user/sigUp', async (req, res) => {
+  const { name, role, phone, email, password,photo } = req.body;
+
+  try {
+    // Check if user already exists
+    const user = await usersCollection.findOne({ email });
+    if (user) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Save the user to the database
+    await usersCollection.insertOne({
+      name,
+      role,
+      phone,
+      email,
+      password: hashedPassword,
+      photo
+    });
+
+    // Generate a JWT token
+    const token = jwt.sign({ email, role }, process.env.ACCESS_TOKEN);
+
+    // Return the token and user details
+    res.status(201).json({ token, email, fullName, role });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ message: 'Failed to register' });
+  }
+});
+
+// User login
+app.post('/user/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if the user exists
+    const user = await usersCollection.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Compare the password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate a JWT token
+    const token = jwt.sign({ email, role: user.role }, process.env.ACCESS_TOKEN);
+
+    // Return the token and user details
+    res.status(200).json({ token, email, name: user.name, role: user.role, photo: user.photo });
+  } catch (error) {
+    console.error('Login error:', error);
+    res.status(500).json({ message: 'Failed to login' });
+  }
+});
+
+// Get user details
+app.get('/user/:email', verifyToken, async (req, res) => {
+  const { email } = req.email;
+
+  try {
+    // Fetch the user details
+    const user = await usersCollection.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Return the user details
+    res.status(200).json({ email: user.email, name: user.name, role: user.role, photo: user.photo });
+  } catch (error) {
+    console.error('User details error:', error);
+    res.status(500).json({ message: 'Failed to fetch user details' });
+  }
+});
+
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
